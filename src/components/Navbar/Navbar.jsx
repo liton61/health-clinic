fab0049ad031ea31aec89fe50b247f75d990dfa7
liton61 <litonofficial61@gmail.com>
2023-10-08 21:43:45 +0600

    responsive navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,11 +13,44 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
-                <img className='w-20' src={logo} alt="" />
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li className='mx-2'><NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Home
+                        </NavLink></li>
+                        <li className='mx-2'><NavLink
+                            to="/doctors"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Doctors
+                        </NavLink></li>
+                        <li className='mx-2'><NavLink
+                            to="/register"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending" : isActive ? "active" : ""
+                            }
+                        >
+                            Register
+                        </NavLink></li>
+                    </ul>
+                </div>
+                <div className="navbar-start">
+                    <img className='w-20' src={logo} alt="" />
+                </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 text-lg font-semibold">
-                    <li><NavLink
+                    <li className='mx-2'><NavLink
                         to="/"
                         className={({ isActive, isPending }) =>
                             isPending ? "pending" : isActive ? "active" : ""
@@ -25,9 +58,7 @@ const Navbar = () => {
                     >
                         Home
                     </NavLink></li>
-                </ul>
-                <ul className="menu menu-horizontal px-1 text-lg font-semibold">
-                    <li><NavLink
+                    <li className='mx-2'><NavLink
                         to="/doctors"
                         className={({ isActive, isPending }) =>
                             isPending ? "pending" : isActive ? "active" : ""
@@ -35,9 +66,7 @@ const Navbar = () => {
                     >
                         Doctors
                     </NavLink></li>
-                </ul>
-                <ul className="menu menu-horizontal px-1 text-lg font-semibold">
-                    <li><NavLink
+                    <li className='mx-2'><NavLink
                         to="/register"
                         className={({ isActive, isPending }) =>
                             isPending ? "pending" : isActive ? "active" : ""
